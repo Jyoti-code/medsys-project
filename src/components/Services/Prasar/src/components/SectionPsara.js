c3598c0f4c0704d600cf14c66beb4c5a62117998
Jyoti-code <jyotisharmarkt2001@gmail.com>
2022-07-07 21:23:59 +0530

    Third commit

---
+++ src/components/Services/Prasar/src/components/SectionPsara.js
@@ -0,0 +1,193 @@
+import React from 'react'
+import FRQ from './FRQ';
+
+function SectionPsara() {
+  return (
+    <>
+<section className="main-content">
+      <div className="left" id="lefty" style={{maxHeight: "250px"}}>
+        <div style={{height: "250px"}} id="activeTableContentaa">
+        <h1>Table Of Contents</h1>
+        <hr/>
+        <p> <a className="tabanchor" href="#overview" >Overview</a></p>
+		<p> <a className="tabanchor" href="#importance">Role of Private Security Agency</a></p>
+		<p> <a className="tabanchor" href="#eligibility">Eligibility Criteria </a></p>
+		
+        <p> <a className="tabanchor" href="#documents">Documents Required</a></p>
+		
+        <p> <a className="tabanchor" href="#attachment">Instructions</a></p>
+		
+        <p> <a className="tabanchor" href="#faq" >FAQs</a></p>
+        <br/>
+        </div>
+        <button type="submit" id="contact"><a href="#contactus"><b style={{color:"white"}}> CONTACT US </b></a></button>
+      </div>
+     
+        {/* <!-- <button type="submit"> <a href="#">Contact Us</a></button> --> */}
+   
+    <div className="right1" id="necessity overview">
+        <div className="necessity">
+          <h1 id="overview">Overview</h1>
+          <br/>
+          <p className="gap">The ever-expanding business landscape in India has escalated the requirement of private security. From banks to lavish restaurants, car showrooms to 
+		  highly-advanced factories, everyone seeks adequate protection to keep criminal elements out of the equation or to ensure proper upkeep of their fences.  This growing safety 
+		  concern has led to the birth of the Private Security Agencies (Regulation) Act, 2005 (PSARA). The said Act offers the leverage of establishing a legalized private security agency in India. 
+		  Besides, this Act governs every aspect of private security agencies in India, be it securing registration or hiring candidates for security services.</p>	  
+          <p id="importance"></p>
+        </div><br/>
+		<iframe width="800" height="385" src="https://www.youtube.com/embed/tgbNymZ7vqY"
+        style={{alignItems: "center", marginLeft: "40px"}}>
+
+      </iframe>
+		<br/><br/>
+        
+        <div className="ICI necessity" id="content">
+          <h1>Fundamental Role of Private Security Agency in India</h1>
+          <br/>
+			
+			<p>Private security agencies refer to a business entity that facilitates security services to businesses from different verticals. 
+			PSARA Act, 2005 came into effect to underpin a certain standard of professionalism in the regime of security services. PSARA license serves as a fundamental mandate for security agencies in India.
+			Apart from facilitating security services, the security agencies offer adequate training to selected candidates for their security-centric careers. </p>
+			<p id="eligibility"></p>
+		 </div>
+		<br/><br/>
+		<div className="ICI necessity">
+          <h1>Eligibility Criteria for Obtaining PSARA License in India</h1>
+          <br/>
+		  <p><b>1: In Terms Of The Type Of Business Organization</b></p><br/>
+		  <p><b style={{color: "black"}}>The Following Business Structures Can Apply For PSARA License</b></p>
+			<ul>
+				<li>Sole Proprietorship, </li>
+				<li>One Person Company(OPC)</li>
+				<li>Partnership Firm,</li>
+				<li>LLP</li>
+				<li>Association of Persons, and</li>
+				<li>Private Limited Company.</li>
+				
+			</ul>
+			<br/>
+		  <p><b>2: In Terms Of The Role Of A Director/Principal Officer</b></p><br/>
+		  <p><b style={{color: "black"}}>The PSARA Act Has Underpinned Specific Criteria For Individuals Intending To Serve The Position Of Director/Principal Officer;</b></p>
+			<ul>
+				<li>Individual must be an Indian national</li>
+				<li>An individual should not be minor as far as age is concerned.</li>
+				<li>Individual must have ample financial resources to cater to business expenses.</li>
+				<li>Individual must have crime-free track records.</li>
+			</ul>
+			<br/>
+		  <p><b>3: In Terms Of The Role Of A Security Guard</b></p><br/>
+		  <p>Individuals intending to become security guards must fulfill the given conditions:</p>
+			<ul>
+				<li>Individual must be an Indian national</li>
+				<li>The person should not be a minor and neither over 65 years of age.</li>
+				<li>The person ought to behave adequate training</li>
+				<li>The person must have a seamless service track record without any termination</li>
+				<li>The person should match the desired physical attributes.</li>
+				<li id="documents">The person must secure character certificate from a previous firm</li>
+			</ul>
+			
+        </div>
+		
+        <br/><br/>
+        <div className="ICI necessity">
+          <h1 >Documents Necessary for Obtaining a PSARA License</h1>
+          <ul>
+			<li>PAN</li>
+			<li>GSTIN</li>
+			<li>Provident Fund Registration</li>
+			<li>ESIC ( i.e. Employees State Insurance Corporation) Registration</li>
+			<li>Registration under Shops and Commercial Establishments Act</li>
+			<li>Registration under the Labour Act</li>
+			<li>Proof relating to the identity of the directors as well as employees</li>
+			<li>Copies of the IT returns of the Directors</li>
+			<li>Two passport size photos and PANs of the promoters</li>
+			<li>Proof relating to the address of the Agency Office</li>
+			<li>Affidavit as u/s 7 (2) of PSARA, 2005</li>
+			<li>Security training affidavit</li>
+			<li>Certificate of Incorporation (COI)</li>
+			<li id="attachment">Signed Memorandum of Understanding i.e. MOU with the authority-recommended training institute</li>
+			</ul>
+        </div>
+		
+		<br/><br/>
+		<div className="ICI necessity">
+          <h1 >Instructions for Securing PSARA License in India</h1>
+          <br/>
+		  <p>Follow the below-mentioned steps for obtaining PSARA license in India:  </p>
+		  <ul>
+			<li>
+              <b >Step 1: Arrange the applicable documents</b> </li> <p >The documents necessary for the application have to be collected and 
+			  sorted accordingly to ensure quick and smooth processing of the application, which will, in turn, speed up the process of obtaining the license.</p><br/>
+			  <p >The documents related to the various registration and details of the directors and promoters have to be legit and in an acceptable format to avert any discrepancy.</p>
+			  <p >Once the required documents are arranged, the application can be filed electronically on the official portal. The presence of the digital signature makes the registration process seamless and hassle-free</p>
+			<li>
+              <b >Step 2: Enter into a Memorandum of Understanding with a certified training institute</b></li>
+			  <p >The applicant must enter into A <b style={{color: "black"}}>Memorandum Of Understanding (MOU) </b>with a certified training institute. The list of the eligible institute is available on the government official portal. These institutes shall provide the required training to the selected candidates.
+			  Ex-servicemen have access to a certain degree of relaxation concerning the training aspects of the agency.</p>
+            
+			<li>
+              <b >Step 3: File The Standard Application</b> </li><p >Once all the above steps are fulfilled, the applicant must file the application (Form-I) with the respective state authority. The said form must be attested
+			  with Form-II (application for verification of antecedents). Affidavit in Form-III must be accompanied by Form-I.</p>
+			
+			<li>
+              <b >Step 4: Complete Formalities Regarding Police Verification</b> </li>
+			  <p >In this step, the applicant is required to file form-I to initiate the Police verification. Where the agency is a company or a partnership, the proposed directors shall 
+			  undergo Police verification without any exception.</p>
+           <li>
+              <b >Step 5: Grant Of PSARA License</b></li>
+			  <p >Where the application is attested with the NOC from the police after verification, the respective authority will scrutinize the same and then proceed
+			  to either accord the license in form-IV or reject the same in case of default.</p>
+		  </ul><br/>
+
+		  <b>Note:</b> Authority usually takes around 60 days to grant this license from the date of application submission. 
+		  Following are the parameters that affect the timeline of granting the license.
+		  <ul>
+			<li><b style={{color: "black"}}>Quantum Of Paperwork Compliance Level</b></li>
+			<li><b style={{color: "black"}}>Type Of License</b></li>
+			<li><b style={{color: "black"}}>Appointment Of Supervisors</b></li>
+		  
+		  </ul>
+		  <p id="faq" >Applicants must appoint the supervisors to stay in line with the provisions of the PSARA, 2005. The supervisors are obligated to administer the agency's operation in relation to the work of the security guards.
+		  The supervisors so appointed must have at least three years of experience in the defence services.</p>
+		  
+        </div>
+		
+		<br/><br/>
+		
+        <div className="faq" >
+          <h1>FAQs</h1>
+          <br/><br/>		
+
+          <section id="about" style={{marginRight : '95px'}}>
+        <hr />
+
+        <div className="accordion" id="faq">
+            <FRQ number={0} ques=" How do I apply for a Psara license?"
+                answer="Steps for apply for Psara license First, arrange the required documentation. Second, Sign MoU with a certified training institute Third, file application in form 1 Fourth, complete the formalities regarding police verification" />
+            <FRQ number={1} ques="Who can apply for Psara?"
+                answer="Indian national or citizen of such other nation as the Central Gov. may, by Official Gazette, specify; Individual who have attained 18 years of age but has not exceeded the age of 65 years; Individuals who duly satisfy the agency about his antecedents & character."/>
+            <FRQ number={2} ques="Is Psara license mandatory for security agency?" 
+                answer="PSARA license is a legal mandate for conducting the business of private security in India. Such businesses facilitate trained security guards to various organizations across the country." />
+            <FRQ number={3} ques="What is the validity of the PSARA License?"
+                answer="Typically, a PSARA license comes with a validity of 5 years which can be extended further by filing the relevant application online. However, in the states like Chhattisgarh & Uttarakhand, it is valid for one year only." />
+            <FRQ number={4} ques="What is the Government fee for PSARA Licence?"
+                answer= "The government fees payable can be divided into the given categories: For one district -- Rs. 5,000/- For 2 to 5 districts in a state -- Rs. 10,000/-. For the entire state -- Rs. 25,000/-."/>
+            <FRQ number={5} ques="What is the role of Supervisor so appointed by the Private security agency in India?"
+                answer="Supervisors are liable to administer the agency’s operation and all the affairs related to security guards." />
+            <FRQ number={6} ques="What are the provisions regarding the License renewal?"
+                answer="The application pertaining to license renewal must be filed 45 days prior to the expiration date." />
+            <FRQ number={7} ques="Is the grant of PSARA license is done on the state-wise basis?"
+                answer="Yes, the grant of PSARA license is done on the state-wise basis." />
+            <FRQ number={8} ques="List down some key documentations for obtaining PSARA license in India?"
+                 answer="MOA, GST registration, MoU with training institute, PF and ESIC Registration, so and so forth."/>
+            <FRQ number={9} ques="Still Confuse?"
+              answer="Speak to our experts who shall solve all your doubts. Call us at +91 70204 38881 or Mail us at medsysconsultancy@gmail.com"/>
+        </div>
+        </section > 
+        </div>
+      </div>
+    </section>
+    </>
+  )
+}
+export default SectionPsara;
